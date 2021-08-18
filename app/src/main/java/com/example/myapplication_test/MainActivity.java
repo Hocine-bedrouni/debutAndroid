ba3f5b7b5c436014ed80eae7ce3ef559c6dd4976
Hocine Bedrouni <hocinebedrouni@gmail.com>
2021-08-18 16:51:33 +0200

    premier commit

--- app/src/main/java/com/example/myapplication_test/MainActivity.java
+++ app/src/main/java/com/example/myapplication_test/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.myapplication_test;
 
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,9 +15,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+////        Ressources res = getResources();
+////        String hw = res.getString(R.string.hello);
+//        String machin = "niktamer";
+////        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+////        builder.setTitle(R.string.hello);
+////        builder.setTitle(R.string.bite);
+////        builder.setTitle(R.string.hello);
+////        Ressources ress = getResources();
+//        String machine = getResources().getString(R.string.hello);
+        Button biteuhn = findViewById(R.id.biteuhn);
+        biteuhn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle(R.string.bite)
+                        .setMessage(R.string.hello)
+                        .setPositiveButton(R.string.ok, ((dialogInterface, i) -> {
+                dialogInterface.dismiss();
+                Intent intent = new Intent(MainActivity.this,MainActivity2.class);
+                startActivity(intent);
+                }))
+                        .show();
+            }
+        });
     }
 
-Ressources res = getResources();
-    String hw = res.getString(R.string.hello);
+//    public void bite()
+//    {
+//        AlertDialog.Builder nomdetonalerte = new AlertDialog.Builder(CONTEXTE.this);// nomdetaclasseoutes.this
+//        nomdetonalerte.setTitle().setMessage().setPositiveButton(nomdetonbouton,(dialogInterface,i)->{code a executer}).setCancelable().show();
+//    }
+
 
 }
---
+++ app/src/main/java/com/example/myapplication_test/MainActivity2.java
@@ -0,0 +1,71 @@
+package com.example.myapplication_test;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+   private TextView result;
+   private EditText edd;
+   private Button btnn;
+   private ImageView digit;
+   private ImageView cc;
+   private TextView pol;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+    result = findViewById(R.id.result);
+    edd = findViewById(R.id.edd);
+    btnn = findViewById(R.id.btnn);
+    digit = findViewById(R.id.digit);
+    cc = findViewById(R.id.cc);
+    pol = findViewById(R.id.pol);
+
+    digit.setVisibility(View.INVISIBLE);
+    cc.setVisibility(View.INVISIBLE);
+    pol.setVisibility(View.INVISIBLE);
+
+    btnn.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+       AlertDialog.Builder alertt = new AlertDialog.Builder(MainActivity2.this);// nomdetaclasseoutes.this
+               alertt.setTitle("Comment va")
+                       .setMessage("Comment ca marche")
+                       .setPositiveButton("non me click pas",(dialogInterface,i)->{dialogInterface.dismiss();
+                       cc.setVisibility(View.VISIBLE);
+                       pol.setVisibility(View.VISIBLE);})
+                       .setNegativeButton("enfonce moi", (dialogInterface, i)->{dialogInterface.dismiss();
+                           AlertDialog.Builder al2 = new AlertDialog.Builder(MainActivity2.this);// nomdetaclasseoutes.this
+                           ImageView ima = new ImageView(MainActivity2.this);
+//                            ima = ima.findViewById(R.id.digit);
+                           ima.setImageResource(R.drawable.logo04);
+                           al2.setView(ima);
+                           al2.setNeutralButton("click-me", new DialogInterface.OnClickListener() {
+                               @Override
+                               public void onClick(DialogInterface dialogInterface, int i) {
+
+                               }
+                           });  al2.show();
+                           digit.setVisibility(View.VISIBLE);
+
+//        if (v == btn) {
+//            result.setText(edd.getText());
+//        }
+    }).show();
+
+    }
+    });}}
